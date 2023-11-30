3ec82afa38eb8f608a403e02142c9af1f62b0093
JamesCLeelayuvat <jmsleelayuvat@gmail.com>
2023-11-30 13:52:00 -0800

    broken code

--- src/main/java/Controller/XMLControllers/StartController.java
+++ src/main/java/Controller/XMLControllers/StartController.java
@@ -1,17 +1,16 @@
 package Controller.XMLControllers;
 
+import View.ViewMaster;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
-public class StartScreenController {
+public class StartController {
 
     @FXML
     private Button signUpButton;
@@ -23,14 +22,12 @@ public class StartScreenController {
     private void handleSignUp(ActionEvent event) {
         try {
             // Load the sign-up screen FXML file
-            Parent signUpScreenRoot = FXMLLoader.load(getClass().getResource("/signUpScreen-view.fxml"));
+            Parent signUpScreenRoot = FXMLLoader.load(getClass().getResource("/signUp-view.fxml"));
 
             // Get the current stage using the button from the current scene
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
+            Stage stage = HelperMethods.getStageFromEvent(event);
             // Set the sign-up screen scene to the stage
-            stage.setScene(new Scene(signUpScreenRoot));
-            stage.show();
+            ViewMaster.setAndShowStage(stage, HelperMethods.createScene(signUpScreenRoot));
         } catch (IOException e) {
             e.printStackTrace();
         }
