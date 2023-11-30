27ac653275e14f93f68a947d0337acd166e6655a
SuperReece <rbdabawss@gmail.com>
2023-11-29 23:29:51 -0800

    added cli

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import View.CLI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,6 +16,8 @@ public class Main extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        CLI cli = new CLI();
+        cli.run();
     }
 
     public static void main(String[] args) {
