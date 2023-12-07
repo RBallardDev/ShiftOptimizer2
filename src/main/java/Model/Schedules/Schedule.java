f3d3df8ddf87073778db647738d1064916aa1961
SuperReece <rbdabawss@gmail.com>
2023-12-07 15:56:50 -0800

    Added days print for schedule

--- src/main/java/Model/Schedules/Schedule.java
+++ src/main/java/Model/Schedules/Schedule.java
@@ -1,47 +1,1 @@
-package Model.Schedules;
-
-import Model.Time.Day;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Schedule {
-    private ArrayList<Day> days = new ArrayList<>();
-
-    public Schedule() {
-        Day.DayNames[] dayNames = {Day.DayNames.Sunday, Day.DayNames.Monday, Day.DayNames.Tuesday,
-                Day.DayNames.Wednesday, Day.DayNames.Thursday, Day.DayNames.Friday,
-                Day.DayNames.Saturday};
-        for (int i = 0; i < 7; i++) {
-            days.add(new Day(dayNames[i]));
-        }
-    }
-
-
-    // Method to check for shift conflicts
-
-    public String printSchedule() {
-        StringBuilder scheduleString = new StringBuilder();
-        for (Day day : days) {
-            for(Shift shift: day.getShifts())
-            scheduleString.append("Shift: ")
-                    .append(shift.getStartTime())
-                    .append(" to ")
-                    .append(shift.getEndTime())
-                    .append("\n");
-        }
-        return scheduleString.toString();
-    }
-
-    public Day getDay(Day.DayNames day){
-        for(int i = 0;i< 7;i++){
-            if(days.get(i).name.equals(day)){
-                return days.get(i);
-            }
-        }
-        throw new IllegalArgumentException();
-    }
-
-    // Additional methods as needed
-}
+package Model.Schedules;import Model.Time.Day;import java.util.ArrayList;import java.util.List;public class Schedule {    private ArrayList<Day> days = new ArrayList<>();    public Schedule() {        Day.DayNames[] dayNames = {Day.DayNames.Sunday, Day.DayNames.Monday, Day.DayNames.Tuesday,                Day.DayNames.Wednesday, Day.DayNames.Thursday, Day.DayNames.Friday,                Day.DayNames.Saturday};        for (int i = 0; i < 7; i++) {            days.add(new Day(dayNames[i]));        }    }    // Method to check for shift conflicts    public String printSchedule() {        StringBuilder scheduleString = new StringBuilder();        for (Day day : days) {            scheduleString.append(day.name).append(":\n");            for(Shift shift: day.getShifts())            scheduleString.append("Shift: ")                    .append(shift.getStartTime())                    .append(" to ")                    .append(shift.getEndTime())                    .append("\n");        }        return scheduleString.toString();    }    private Day getDay(Day day) {        for(int i = 0;i< 7;i++){            day = days.get(i);        }        return day;    }    public Day getDay(Day.DayNames day){        for(int i = 0;i< 7;i++){            if(days.get(i).name.equals(day)){                return days.get(i);            }        }        throw new IllegalArgumentException();    }    // Additional methods as needed}
