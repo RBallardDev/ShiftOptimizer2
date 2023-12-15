ae526c987f86ae7c27a1d98b193f995ef8b95c09
SuperReece <rbdabawss@gmail.com>
2023-12-15 01:16:02 -0800

    worker view schedule
    i think this is working but im not totally sure bc there isnt anyrhing to test with in JSON

--- src/main/java/View/XMLControllers/WorkerScheduleViewController.java
+++ src/main/java/View/XMLControllers/WorkerScheduleViewController.java
@@ -1,6 +1,15 @@
 package View.XMLControllers;
 
+import Controller.File.JacksonGetter;
+import Controller.ShiftAssignment.ShiftAssigner;
 import Controller.Time.Week;
+import Controller.UserAuth.Session;
+import Controller.UserAuth.SessionAuth;
+import Model.Schedules.OptimizedSchedule.Hour;
+import Model.Schedules.OptimizedSchedule.OptimizedDaySchedule;
+import Model.Schedules.OptimizedSchedule.OptimizedSchedule;
+import Model.Staff.Worker;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +18,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
+
+
 import java.io.IOException;
+import java.time.LocalTime;
 
 public class WorkerScheduleViewController {
 
@@ -38,10 +50,47 @@ public class WorkerScheduleViewController {
 
     @FXML
     private void initialize() {
-        // Load and display the worker's schedule
-        // This is where you fetch the schedule data and populate the ListViews
-        // Example for Sunday (repeat for other days)
-        //sundayListView.getItems().addAll("9:00 - 12:00", "14:00 - 17:00"); // Replace with actual data
+        ShiftAssigner.optimizeWeek();
+        Worker currentWorkerUsername = JacksonGetter.getWorkerByUsername(SessionAuth.authenticateToken(Session.getToken()));
+        for (int i = 0; i < 7; i++) {
+            OptimizedDaySchedule optimizedDaySchedule = OptimizedSchedule.optimizedDaySchedules[i];
+
+            LocalTime startTime = null;
+            LocalTime endTime = null;
+            String username = null;
+
+            for (Hour hour : optimizedDaySchedule.getHours()) {
+                for (int halfHour = 0; halfHour < 2; halfHour++) {
+                    String currentUsername = hour.halfHours[halfHour];
+                    LocalTime currentTime = LocalTime.of(hour.hourOfDay, halfHour * 30);
+
+                    // Check if the current shift belongs to the signed-in worker
+                    if (currentUsername != null && currentUsername.equals(currentWorkerUsername)) {
+                        if (username == null) {
+                            // Start of a new shift
+                            startTime = currentTime;
+                            username = currentUsername;
+                        } else if (!currentUsername.equals(username)) {
+                            // End of the current shift
+                            endTime = currentTime;
+                            addShiftToDayListView(optimizedDaySchedule.name, username, startTime, endTime);
+                            startTime = currentTime; // Start of a new shift
+                        }
+                    }
+                }
+            }
+
+            // Add the last shift if it hasn't been added
+            if (username != null) {
+                endTime = LocalTime.of(23, 59); // Assuming the last possible time for a shift
+                addShiftToDayListView(optimizedDaySchedule.name, username, startTime, endTime);
+            }
+        }
+    }
+
+    private void addShiftToDayListView(Week.DayNames dayName, String username, LocalTime startTime, LocalTime endTime) {
+        ObservableList<String> items = getDayListView(dayName).getItems();
+        items.add(username + ": " + startTime + " - " + endTime);
     }
 
     private ListView<String> getDayListView(Week.DayNames dayName) {
